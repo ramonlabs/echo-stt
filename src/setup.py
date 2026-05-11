f62b40717b969dc7b7b727c1cd2486d810750e4b
Ramon Asuncion <asuncionbatista@gmail.com>
2026-05-11 14:45:56 -0400

    Add setup.py

---
+++ src/setup.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+from venv import create
+from subprocess import run
+
+venv_dir = "./stt-venv"
+pip_path = os.path.join(venv_dir, "bin", "pip")
+
+if not os.path.isdir(venv_dir):
+    create(venv_dir, with_pip=True)
+    run([pip_path, "install", "-r", os.path.abspath("requirements.txt")], check=True)
+
+os.makedirs("private", exist_ok=True)
+
+if not os.path.isfile("private/config.yaml"):
+    shutil.copyfile("templates/config.yaml.example", "private/config.yaml")
