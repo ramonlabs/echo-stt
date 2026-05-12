23a93b1803a54cdd0c9c0753cd0a887c86ca79d4
Ramon Asuncion <asuncionbatista@gmail.com>
2026-05-12 15:37:40 -0400

    Ignore exceptions

--- src/setup.py
+++ src/setup.py
@@ -6,6 +6,7 @@ from subprocess import run
 venv_dir = "./stt-venv"
 pip_path = os.path.join(venv_dir, "bin", "pip")
 
+# TODO(d809): re-sync packages if requirements.txt changes after venv is created
 if not os.path.isdir(venv_dir):
     create(venv_dir, with_pip=True)
     run([pip_path, "install", "-r", os.path.abspath("requirements.txt")], check=True)
